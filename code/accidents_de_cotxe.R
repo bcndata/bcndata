a10ae5104a169630a59aec6df94269106a158687
Joe Brew <joebrew@gmail.com>
2015-11-30 07:02:16 -0500

    minor changes

--- code/accidents_de_cotxe.R
+++ code/accidents_de_cotxe.R
@@ -1,5 +1,5 @@
 # ##### Directories 
-root <- '/home/joebrew/Documents/bcn_data'
+root <- '/home/joebrew/Documents/bcndata'
 data_dir <- paste0(root, '/data/accidents_de_cotxe')
 code_dir <- paste0(root, '/code')
 
@@ -8,8 +8,12 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
+library(rgdal)
 # require(RCurl)
 # options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+
+##### Theme
+source(paste0(root, '/lib/theme.R'))
 
 ##### Data read in
 csvs <- dir(data_dir)
@@ -32,3 +36,129 @@ for (i in 1:length(csvs)){
 }
 accidents <- do.call('rbind', results_list)
 rm(temp, results_list, csvs, hh, i)
+
+# Numeric coordiantes
+accidents$y <- as.numeric(gsub(',', '.', accidents$Coordenada.UTM..Y.))
+accidents$x <- as.numeric(gsub(',', '.', accidents$Coordenada.UTM..X. ))
+
+##### Geographic data
+mapa <- readOGR(paste0(root, '/data/geo/barris'), 'BCN_Barri_SHP')
+
+
+#####
+# LOCATION
+#####
+
+# Red for any death, orange for any serious injury
+accidents$color <- 
+  ifelse(accidents$Número.de.morts > 0, 
+         adjustcolor('red', alpha.f = 0.8),
+         ifelse(accidents$Número.de.lesionats.greus > 0,
+                adjustcolor('darkorange', alpha.f = 0.5),
+                ifelse(accidents$Número.de.lesionats.lleus > 0,
+                       adjustcolor('blue', alpha.f = 0.3),
+                       adjustcolor('green', alpha.f = 0.2))))
+
+# Size
+accidents$size <-
+  ifelse(accidents$Número.de.morts > 0, 
+         0.6,
+         ifelse(accidents$Número.de.lesionats.greus > 0,
+                0.4,
+                ifelse(accidents$Número.de.lesionats.lleus > 0,
+                       0.2, 0.1)))
+
+# remove points outside of bbox
+accidents <- 
+  accidents[which(
+    accidents$x >= data.frame(bbox(mapa))['min'][1,] &
+      accidents$x <= data.frame(bbox(mapa))['max'][1,] &
+      accidents$y >= data.frame(bbox(mapa))['min'][2,] &
+      accidents$y <= data.frame(bbox(mapa))['max'][2,]),]
+
+# Order so that green comes first, red last
+
+par(mar = c(0,0,0,0))
+jpeg('/home/joebrew/Documents/bcndata.github.io/img/2015-11-30-Car-accidents-in-Barcelona/points2.JPG',
+     width = 700,
+     height = 500)
+plot(mapa, 
+     col = adjustcolor('beige', alpha.f = 0.9),
+     border = NA)#adjustcolor('black', alpha.f = 0.2))
+points(accidents$x, accidents$y, pch = 16,
+       cex = accidents$size,
+       col = accidents$color)
+dev.off()
+par(mar = c(4.1, 2.1, 2.1, 1.1))
+
+mapa_f <- fortify(mapa)
+
+ggplot(mapa_f, aes(long, lat,group=group)) + 
+  geom_polygon() +
+  geom_path(color="white") +
+  coord_equal() +
+  theme(axis.line=element_blank(),axis.text.x=element_blank(),
+        axis.text.y=element_blank(),axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),legend.position="none",
+        panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),plot.background=element_blank()) +
+  geom_point(data = accidents, 
+             aes(x = x, y = y, color = color, size = 0.2 * size, group = NA), alpha = 0.2)
+ggsave('/home/joebrew/Documents/bcndata.github.io/img/2015-11-30-Car-accidents-in-Barcelona/points.JPG')
+
+ggplot(mapa_f, aes(long, lat,group=group)) + 
+  geom_polygon(fill = 'grey') +
+  geom_path(color="white") +
+  coord_equal() +
+  theme(axis.line=element_blank(),axis.text.x=element_blank(),
+        axis.text.y=element_blank(),axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),legend.position="none",
+        panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),plot.background=element_blank()) +
+  #   geom_point(data = accidents, 
+  #               aes(x = x, y = y, color = color, size = 0.2 * size, group = NA), alpha = 0.2) +
+  stat_density2d(aes(alpha=..level..), geom="polygon", fill = 'red') +
+  scale_alpha_continuous(limits=c(0,0.2),breaks=seq(0,0.2,by=0.025))
+ggsave('/home/joebrew/Documents/bcndata.github.io/img/2015-11-30-Car-accidents-in-Barcelona/hotspots.JPG')
+
+#####
+# TIME OF DAY
+#####
+
+
+# TIME OF DAY
+tod <- 
+  accidents %>%
+  group_by(Hora.de.dia) %>%
+  summarise(n = n())
+ggplot(data = tod, aes(x = Hora.de.dia, y = n / 5)) +
+  geom_point() +
+  geom_smooth() +
+  xlab('Time of day') +
+  ylab('Accidents') +
+  bcn_data_theme +
+  ggtitle('Don\'t drive at 3pm')
+rm(tod)
+ggsave('/home/joebrew/Documents/bcndata.github.io/img/2015-11-30-Car-accidents-in-Barcelona/hora.JPG')
+
+#####
+# DAY OF WEEK
+#####
+
+dow <- 
+  accidents %>%
+  group_by(Descripció.dia.setmana) %>%
+  summarise(n = n())
+dow$Descripció.dia.setmana <- 
+  factor(as.character(dow$Descripció.dia.setmana),
+         levels = c('Dilluns', 'Dimarts', 'Dimecres', 'Dijous',
+                    'Divendres', 'Dissabte', 'Diumenge'))
+ggplot(data = dow, aes(x = Descripció.dia.setmana, y = n)) +
+  geom_bar(stat = 'identity', fill = 'red', alpha = 0.6) +
+  bcn_data_theme +
+  ggtitle('Stay off the road on Friday') +
+  xlab('Day of the week') +
+  ylab('Accidents') +
+ggsave('/home/joebrew/Documents/bcndata.github.io/img/2015-11-30-Car-accidents-in-Barcelona/dia.JPG')
--- lib/theme.R
+++ lib/theme.R
@@ -3,6 +3,19 @@ library(ggplot2)
 # Set the default theme
 theme_set(theme_bw())
 # Modify it with theme()
-tt <- theme(axis.text = element_text(size=18, colour=NULL))
+tt <- theme(axis.text = element_text(size=18, colour=NULL),
+            axis.title = element_text(size = 24, colour= NULL),
+            plot.title = element_text(size = 30),
+            # axis.line = element_line(size = 1, colour = "black"),
+            axis.text = element_text(colour = "blue"),
+            axis.ticks = element_line(size = 2),
+            legend.background = element_rect(fill = "white"),
+            #legend.position = c(0.14, 0.80),
+            panel.grid.major = element_line(colour = "grey80"),
+            panel.grid.minor = element_blank(),
+            panel.background = element_rect(fill = "white"),
+            panel.border = element_rect(colour = "black", fill=NA, size=1),
+            text = element_text(size = 12, family = 'Times New Roman')
+            )
 
 bcn_data_theme <- tt
